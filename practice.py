a82460ec1d3f9bac256bda56f4f73b1743790613
Uday Malhotra <69810354+urmalhotra@users.noreply.github.com>
2022-03-23 20:28:37 -0500

    Neural Net from scratch w/o libraries
    In progress.

---
+++ practice.py
@@ -0,0 +1,50 @@
+import numpy as np    
+''' weights = [[0.2, 0.8, -0.5, 1.0],
+            [0.5, -0.91, 0.26, -0.5],
+            [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]    
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]  
+layer_outputs = []
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_output = 0
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_output += n_input*weight
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
+
+layer1_output = np.dot(inputs, np.array(weights).T) + biases
+layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases2 '''
+
+np.random.seed(0)
+
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(4,5)
+layer2 = Layer_Dense(5,2)
+
+layer1.forward(X)
+#print(layer1.output)
+activation1 = Activation_ReLU()
+activation1.forward(layer1.output)
+layer2.forward(activation1.output)
+activation1.forward(layer2.output)
+print(activation1.output) 
